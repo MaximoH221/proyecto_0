9ec18e2390466b9bcafc21801f79da4e189b219e
Máximo <maxiedileal3221@gmail.com>
2026-09-15 13:48:20 -0300

    Agrego algunos archivos...

---
+++ Prueba_1.py
@@ -0,0 +1,19 @@
+#Aclaro que primero que todo no encuentro en la pagina metereologica 
+#Observaciones actuales, donde debería incidir el archivo.
+#por lo que primeramente lo haré con el archivo que nos da el profe
+#en el repositorio de github, y luego lo haré con el archivo que se encuentra en la pagina metereologica.
+#(si es que lo encuentro)
+#gracias
+#Segundo para aclarar, no se si falte a la clase, o si no lo vimos
+#pero como el profe pide para el proyecto, cierto comando 
+#para abrir el archivo y no se que cosa, lo saque de la inteligencia artificial,
+#no para copiarme ni nada, solo para saber que era, y me dijo que
+#necesitaba importar la libreria sys, para que el archivo ande con
+#el comando :), muchas gracias por leer.
+#algo así me dijo la inteligencia que se hacía:
+#import sys
+#sys.argv[1]
+#ruta= sys.argv[1]
+#with open(ruta, "r") as f:
+#y ahí después se procesan los datos..
+
